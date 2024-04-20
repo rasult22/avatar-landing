386de4f8a82f8b120b238ad002f56a9753f89f27
Itachi-MacOS <warrior.006@inbox.ru>
2024-04-20 16:51:39 +0500

    avatar landing

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
+  site: "http://avatar-landing-page-site.s3-website-us-east-1.amazonaws.com/",
   integrations: [tailwind(), icon(), svelte()],
 });
