bac632a4f084231939458e84bdeea057cfb0a600
Itachi-MacOS <warrior.006@inbox.ru>
2024-06-03 01:27:01 +0500

    whitelist

--- src/components/login.svelte
+++ src/components/login.svelte
@@ -30,22 +30,22 @@
   Login
 </button>
 {#if show}
-  <div transition:fade class="bg-[#091720] w-[100vw] h-[100vh] fixed left-0 top-0">
-    <div class="max-w-[1264px] mx-auto">
-        <div class="flex justify-between py-12">
+  <div transition:fade class="bg-[#091720] w-[100vw] h-[100vh] fixed left-0 top-0 sm:translate-x-[-20%]">
+    <div class="max-w-[1264px] mx-auto sm:px-4 md:px-6">
+        <div class="flex justify-between py-12 sm:py-6 items-center">
           <img class="max-w-[206px]" width="206" src="/optimized/logo.webp" alt="logo">
-          <svg class="cursor-pointer" on:click={toggleShow} xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 45 45" fill="none">
+          <svg class="cursor-pointer sm:w-[32px]" on:click={toggleShow} xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 45 45" fill="none">
             <path d="M11.25 33.75L33.75 11.25M11.25 11.25L33.75 33.75" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
           </svg>
         </div>
       {#if !sumbitted}
-        <div class="flex mt-[72px] items-center justify-center space-x-[110px] px-[80px]">
+        <div class="flex mt-[72px] sm:mt-[2vh] items-center justify-center space-x-[110px] sm:px-0 px-[80px]">
           <div>
-            <div class="text-[48px] font-semibold leading-[120%]">
+            <div class="text-[48px] sm:text-[32px] sm:text-center font-semibold leading-[120%]">
               Whitelist <br/>
               application
             </div>
-            <div class="text-[20px] mt-[16px]">
+            <div class="text-[20px] sm:text-center sm:text-[17px] mt-[16px]">
               Get access to avatars one of the first!
             </div>
             <form on:submit|preventDefault={onSubmit} class="rounded-lg mt-[24px] w-full max-w-sm">
@@ -73,16 +73,21 @@
               </p>
             </form>
           </div>
-          <div class="">
-            <img class="max-w-[496px]" width="496" src="/whitelist-img.png" alt="">
+          <div class="sm:hidden md:hidden">
+            <img class="max-w-[496px]" width="496" src="/optimized/whitelist-img.webp" alt="">
           </div>
         </div>
       {:else}
         <div class="flex flex-col min-h-[80vh] justify-center py-12 items-center">
-          <div class="text-[48px] font-semibold leading-[120%]">
+          <div class="text-[48px] sm:text-[24px] font-semibold leading-[120%]">
             Application submitted!
           </div>
-          <button on:click={toggleShow} class="mt-8 px-8 disabled:opacity-55 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+          <button on:click={() => {
+            toggleShow()
+            sumbitted = false
+            name = ''
+            email = ''
+            }} class="mt-8 px-8 disabled:opacity-55 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
             Ok
           </button>
         </div>
